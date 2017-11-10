c63c8e7e09eb9eb6a9da9bbab1df0a44696b53fe
llt <llt_951118@163.com>
2017-11-10 15:18:14 +0800

    Initial commit using new project wizard

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,12 +2,29 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RemindersActivity extends AppCompatActivity {
-
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        mListView = (ListView)findViewById(R.id.reminders_list_view);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(
+                //context
+                this,
+                //layout (view)
+                R.layout.reminders_row,
+                //row(view)
+                R.id.row_text,
+                //data(model) with bogus data to test our listview
+                new String[]{"first record","second record","third record"}
+        );
+        mListView.setAdapter(arrayAdapter);
     }
 }
